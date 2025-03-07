024f6a97908e3832192b6750d6bf244ba97604bd
rgubarev <rgubarev@astralinux.ru>
2025-03-07 19:39:32 +0300

    Продолжаем изучать го

--- hello.go
+++ hello.go
@@ -2,9 +2,17 @@ package main
 
 import "fmt"
 
+type Car struct {
+	Model string
+	Speed int
+}
+
+// Функция для проверки четности числа
 func isEven(n int) bool {
 	return n%2 == 0
 }
+
+// Функция для проверки четности числа
 func multiply(a int, b int) int {
 	return a * b
 }
@@ -12,6 +20,7 @@ func multiply(a int, b int) int {
 func main() {
 	fmt.Println("Hello, World!")
 
+	// Проверка четности чисел от 0 до 4
 	for i := range 5 {
 		if isEven(i) {
 			fmt.Println(i, ": четное число")
@@ -19,4 +28,11 @@ func main() {
 			fmt.Printf("%d: нечетное число\n", i)
 		}
 	}
+
+	a, b := 3, 4
+	result := multiply(a, b)
+	fmt.Printf("Результат умножения %d * %d = %d\n", a, b, result)
+
+	mycar := Car{"Tesla", 320}
+	fmt.Printf("Марка машины: %s, скорость: %d км/ч\n", mycar.Model, mycar.Speed)
 }
